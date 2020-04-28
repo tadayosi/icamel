ffd4403fd12b01bfc21be1eade87d654aca7f570
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2020-04-28 12:06:49 +0900

    fix: support Java

--- src/main/java/io/github/tadayosi/icamel/LanguageResolver.java
+++ src/main/java/io/github/tadayosi/icamel/LanguageResolver.java
@@ -1,7 +1,10 @@
 package io.github.tadayosi.icamel;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.camel.k.Source;
 import org.apache.camel.k.Sources;
@@ -10,17 +13,34 @@ public class LanguageResolver {
 
     public static final String DEFAULT_LANGUAGE = "js";
 
+    private static final Pattern CLASS_PATTERN =
+        Pattern.compile("^\\s*public\\s+class\\s+([a-zA-Z_$][a-zA-Z_$\\d]*)\\s*.*$");
+
     public LanguageResolver() {
     }
 
     public Source resolve(String expr) {
+        Optional<String> language = resolveLanguage(expr);
+        if (language.filter("java"::equals).isPresent()) {
+            return Sources.fromBytes(
+                findJavaClassName(expr),
+                language.get(),
+                null,
+                expr.getBytes(StandardCharsets.UTF_8));
+        }
+        return Sources.fromBytes(
+            language.orElse(DEFAULT_LANGUAGE),
+            expr.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private Optional<String> resolveLanguage(String expr) {
+        Optional<String> language = Optional.empty();
         int eol = expr.indexOf(System.lineSeparator());
         if (eol < 0) {
             // one-liner
-            return Sources.fromBytes(DEFAULT_LANGUAGE, expr.getBytes(StandardCharsets.UTF_8));
+            return language;
         }
         String begin = expr.substring(0, eol);
-        Optional<String> language = Optional.empty();
         if (begin.startsWith("//") && begin.contains("language=")) {
             // check comment
             language = Optional.of(begin.trim().split("=")[1]);
@@ -31,6 +51,15 @@ public class LanguageResolver {
             // yaml
             language = Optional.of("yaml");
         }
-        return Sources.fromBytes(language.orElse(DEFAULT_LANGUAGE), expr.getBytes(StandardCharsets.UTF_8));
+        return language;
+    }
+
+    private String findJavaClassName(String expr) {
+        return Arrays.stream(expr.split(System.lineSeparator()))
+            .map(CLASS_PATTERN::matcher)
+            .filter(Matcher::find)
+            .map(m -> m.group(1))
+            .findFirst()
+            .orElseThrow(() -> new RuntimeException("Java source requires public class."));
     }
 }
--- src/test/java/io/github/tadayosi/icamel/LanguageResolverTest.java
+++ src/test/java/io/github/tadayosi/icamel/LanguageResolverTest.java
@@ -1,21 +1,35 @@
 package io.github.tadayosi.icamel;
 
+import org.apache.camel.k.Source;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LanguageResolverTest {
 
+    private LanguageResolver resolver;
+
+    @Before
+    public void setUp() {
+        resolver = new LanguageResolver();
+    }
+
     @Test
     public void resolve() {
-        LanguageResolver resolver = new LanguageResolver();
-        assertThat(resolver.resolve("// language=groovy" + System.lineSeparator()).getLanguage()).isEqualTo("groovy");
+        assertThat(resolver.resolve("// language=groovy\n").getLanguage()).isEqualTo("groovy");
         assertThat(resolver.resolve("// language=js\n").getLanguage()).isEqualTo("js");
-        assertThat(resolver.resolve("// language=java\n").getLanguage()).isEqualTo("java");
         assertThat(resolver.resolve("// language=kts\n").getLanguage()).isEqualTo("kts");
         assertThat(resolver.resolve("// language=xml\n").getLanguage()).isEqualTo("xml");
         assertThat(resolver.resolve("// language=yaml\n").getLanguage()).isEqualTo("yaml");
         // default
         assertThat(resolver.resolve("console.log('hello')").getLanguage()).isEqualTo("js");
+    }
+
+    @Test
+    public void resolveJava() {
+        Source source = resolver.resolve("// language=java\n" + "public class Test {}");
+        assertThat(source.getLanguage()).isEqualTo("java");
+        assertThat(source.getName()).isEqualTo("Test");
     }
 }
